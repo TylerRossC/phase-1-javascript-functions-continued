4d8f787a8c9ed08c65e960e3944dd9ed20fed894
tyler <tylercharawell@gmail.com>
2021-04-28 10:18:20 -0700

    commit tree d61380d165c93fce5c5d6f5e10a5162b0bf9dc15

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+//expect(saturdayFun()).to.equal("This Saturday, I want to roller-skate!")
+//expect(saturdayFun("bathe my dog")).to.equal("This Saturday, I want to bathe my dog!")
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+}
+//saturdayFun('bathe my dog') this overrides 'roller-skate'
+
+// expect(mondayWork()).to.equal("This Monday, I will go to the office.")
+//expect(mondayWork("work from home")).to.equal("This Monday, I will work from home.")
+function mondayWork(work = 'go to the office'){
+    return (`This Monday, I will ${work}.`);
+}
+
+function wrapAdjective(char = '*'){
+    return function(hello = 'special'){
+        return (`You are ${char}${hello}${char}!`);
+    }
+}
+//wrapAdjective('*')('a hard worker')
